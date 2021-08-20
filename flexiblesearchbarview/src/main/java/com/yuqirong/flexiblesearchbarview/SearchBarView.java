743b7d8f1af74063454945789c81be968e4db4dd
veejay <veejaynext@gmail.com>
2021-08-20 10:36:38 +0530

    changed author name in lib file

--- flexiblesearchbarview/src/main/java/com/yuqirong/flexiblesearchbarview/SearchBarView.java
+++ flexiblesearchbarview/src/main/java/com/yuqirong/flexiblesearchbarview/SearchBarView.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 /**
- * SearchBarView class is Custom View.
+ * @author qirong
  */
 public class SearchBarView extends Component implements Component.DrawTask, Component.EstimateSizeListener {
     /**
